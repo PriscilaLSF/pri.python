d2ccfa88d147f8e4c9a973b1c4c1a431190d0bed
PriscilaLSF <102425790+PriscilaLSF@users.noreply.github.com>
2022-04-12 20:03:20 -0300

    Add files via upload

---
+++ pri.py
@@ -0,0 +1,6 @@
+x=input("Insira seu primeiro nome")
+y=input("Insira seu salario do mes de abril")
+Parte1y:y[0:3]
+parte2y:y[3:6]
+parte3y:y[6:7]
+print("O salário de"+ x + "foi de R$" + Parte1y + parte2y + "," +parte3y + "no mês de Abril")
